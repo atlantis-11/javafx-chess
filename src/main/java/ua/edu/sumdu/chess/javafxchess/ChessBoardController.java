10cbf8fe7f3787533a8b659dcc52345d21d552c6
Тимофей Красько <timakrasko2004@gmail.com>
2024-03-09 12:09:11 +0200

    Split the methods into parts

--- src/main/java/ua/edu/sumdu/chess/javafxchess/ChessBoardController.java
+++ src/main/java/ua/edu/sumdu/chess/javafxchess/ChessBoardController.java
@@ -34,91 +34,53 @@ public class ChessBoardController {
     @Getter
     @Setter
     private Game game;
-    private Color blackSquare = Color.rgb(119, 153, 84);
-    private Color whiteSquare = Color.rgb(233, 237, 204);
-    private Color stepColor = Color.rgb(244, 244, 128, 0.5);
-
-    @FXML
-    void moveClick(MouseEvent event) {
-        int row = (int) (event.getY() / (boardGridPane.getHeight() / 8));
-        int col = (int) (event.getX() / (boardGridPane.getWidth() / 8));
-        Position pos = new Position(row, col);
-
-
-        if (selectedPos == null) {
-            List<Move> legalMoves = game.getLegalMoves(pos);
-
-            if (!legalMoves.isEmpty()) {
-                selectedPos = pos;
-
-                for (Move move : legalMoves) {
-                    int rowLM = move.getTo().row();
-                    int colLM = move.getTo().col();
-                    Circle circle = new Circle();
-                    StackPane stackPane = squareStackPanes[rowLM][colLM];
-
-                    if (((stackPane.getChildren().size() > 1) &&
-                            !(stackPane.getChildren().get(1) instanceof Rectangle)||
-                            stackPane.getChildren().size() > 2)){
-                        circle.getStyleClass().add("legalMoveWithCapture");
-                    } else {
-                        circle.getStyleClass().add("legalMove");
-                    }
-
-                    squareStackPanes[rowLM][colLM].getChildren().add(circle);
-                }
-
-                Platform.runLater(() -> adjustSquareSize(boardPane.getWidth(), boardPane.getHeight()));
-            }
-        } else {
-            PieceType pieceType = tryParsePieceType("\n");
-
-            game.makeMove(selectedPos, pos, pieceType);
-
-            selectedPos = null;
-        }
-
-    }
-
-    private PieceType tryParsePieceType(String pieceTypeString) {
-        try {
-            return PieceType.valueOf(pieceTypeString.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            return PieceType.QUEEN;
-        }
-    }
+    private final Color blackSquare = Color.rgb(119, 153, 84);
+    private final Color whiteSquare = Color.rgb(233, 237, 204);
+    private final Color stepColor = Color.rgb(244, 244, 128, 0.5);
 
     @FXML
     void initialize() {
+        initializeSquareStackPanes();
+        addBoardListeners();
+        Platform.runLater(() -> adjustSquareSize(boardPane.getWidth(),
+                boardPane.getHeight()));
+    }
+
+    private void initializeSquareStackPanes() {
         squareStackPanes = new StackPane[8][8];
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
-                StackPane squareStackPane = new StackPane();
-                squareStackPanes[row][col] = squareStackPane;
+                StackPane squareStackPane = createSquareStackPane(row, col);
                 boardGridPane.add(squareStackPane, col, row);
-
-                Rectangle rectangle = new Rectangle();
-                rectangle.setFill((row + col) % 2 == 0 ? whiteSquare : blackSquare);
-
-                squareStackPane.getChildren().add(rectangle);
+                squareStackPanes[row][col] = squareStackPane;
             }
         }
+    }
 
-        boardPane.widthProperty().addListener((observableValue, oldWidth, newWidth) -> {
+    private StackPane createSquareStackPane(int row, int col) {
+        StackPane squareStackPane = new StackPane();
+        Rectangle rectangle = new Rectangle();
+        rectangle.setFill((row + col) % 2 == 0 ? whiteSquare : blackSquare);
+        squareStackPane.getChildren().add(rectangle);
+        return squareStackPane;
+    }
+
+    private void addBoardListeners() {
+        boardPane.widthProperty().addListener((observableValue,
+                                               oldWidth, newWidth) -> {
             double height = boardPane.getHeight();
             if (newWidth.doubleValue() / height > 1) {
                 adjustSquareSize(newWidth.doubleValue(), height);
             }
         });
 
-        boardPane.heightProperty().addListener((observableValue, oldHeight, newHeight) -> {
+        boardPane.heightProperty().addListener((observableValue,
+                                                oldHeight, newHeight) -> {
             double width = boardPane.getWidth();
             if (newHeight.doubleValue() / width > 1) {
                 adjustSquareSize(width, newHeight.doubleValue());
             }
         });
-
-        Platform.runLater(() -> adjustSquareSize(boardPane.getWidth(), boardPane.getHeight()));
     }
 
     public void drawBoard() {
@@ -135,30 +97,39 @@ public class ChessBoardController {
                 Position position = new Position(i, j);
                 Piece piece = board.getPiece(position);
 
-                int squareSize = squareStackPane.getChildren().size();
-                if (squareSize > 1) {
-                    squareStackPane.getChildren().remove(1, squareStackPane.getChildren().size());
+                if (squareStackPane.getChildren().size() > 1) {
+                    squareStackPane.getChildren().
+                            remove(1, squareStackPane.getChildren().size());
                 }
 
-                if ((position.equals(from)) || (position.equals(to))) {
-                    StackPane squareStackPanelLM = squareStackPanes[i][j];
-                    Rectangle rectangleLM = new Rectangle();
-                    rectangleLM.setFill(stepColor);
-
-                    squareStackPanelLM.getChildren().add(rectangleLM);
-                }
-
-                if (piece != null) {
-                    ImageView pieceView = new ImageView();
-                    squareStackPane.getChildren().add(pieceView);
-
-                    Platform.runLater(() -> adjustSquareSize(boardPane.getWidth(), boardPane.getHeight()));
-
-                    pieceView.setImage(new Image(getImagePath(piece)));
-
-                }
-
+                highlightLastMove(squareStackPane, position, from, to);
+                drawPiece(squareStackPane, piece);
             }
+        }
+    }
+
+    private void highlightLastMove(StackPane squareStackPane,
+                                   Position position, Position from,
+                                   Position to) {
+        if ((position.equals(from)) || (position.equals(to))) {
+            Rectangle rectangleLastMove = new Rectangle();
+            rectangleLastMove.setFill(stepColor);
+
+            squareStackPane.getChildren().add(rectangleLastMove);
+        }
+    }
+
+    private void drawPiece(StackPane squareStackPane, Piece piece) {
+        if (piece != null) {
+            ImageView pieceView = new ImageView();
+            pieceView.setFitWidth(1);
+            pieceView.setFitHeight(1);
+            squareStackPane.getChildren().add(pieceView);
+
+            Platform.runLater(() -> adjustSquareSize(boardPane.getWidth(),
+                    boardPane.getHeight()));
+
+            pieceView.setImage(new Image(getImagePath(piece)));
         }
     }
 
@@ -168,8 +139,80 @@ public class ChessBoardController {
             default -> piece.getType().name().substring(0, 1).toLowerCase();
         };
 
-        return (piece.getColor() == PieceColor.WHITE ? "w" : "b") + symbol + ".png";
+        return (piece.getColor() == PieceColor.WHITE ? "w" : "b")
+                + symbol + ".png";
     }
+
+    @FXML
+    void moveClick(MouseEvent event) {
+        Position pos = getPositionFromMouseEvent(event);
+
+        if (selectedPos == null) {
+            handleSelection(pos);
+        } else {
+            handleMove(pos);
+        }
+    }
+
+    private Position getPositionFromMouseEvent(MouseEvent event) {
+        int row = (int) (event.getY() / (boardGridPane.getHeight() / 8));
+        int col = (int) (event.getX() / (boardGridPane.getWidth() / 8));
+        return new Position(row, col);
+    }
+
+    private void handleSelection(Position pos) {
+        List<Move> legalMoves = game.getLegalMoves(pos);
+
+        if (!legalMoves.isEmpty()) {
+            selectedPos = pos;
+
+            for (Move move : legalMoves) {
+                addLegalMoves(move);
+            }
+
+            Platform.runLater(() -> adjustSquareSize(boardPane.getWidth(),
+                    boardPane.getHeight()));
+        }
+    }
+
+    private void addLegalMoves(Move move) {
+        int rowLM = move.getTo().row();
+        int colLM = move.getTo().col();
+        Circle circle = createLegalMoves(move);
+
+        squareStackPanes[rowLM][colLM].getChildren().add(circle);
+    }
+
+    private Circle createLegalMoves(Move move) {
+        Circle circle = new Circle();
+        int rowLegalMove = move.getTo().row();
+        int colLegalMove = move.getTo().col();
+        StackPane stackPane = squareStackPanes[rowLegalMove][colLegalMove];
+
+        if (((stackPane.getChildren().size() > 1) &&
+                !(stackPane.getChildren().get(1) instanceof Rectangle) ||
+                stackPane.getChildren().size() > 2)) {
+            circle.getStyleClass().add("legalMoveWithCapture");
+        } else {
+            circle.getStyleClass().add("legalMove");
+        }
+
+        return circle;
+    }
+
+    private void handleMove(Position pos) {
+//        PieceType pieceType = tryParsePieceType("\n");
+        game.makeMove(selectedPos, pos);
+        selectedPos = null;
+    }
+
+//    private PieceType tryParsePieceType(String pieceTypeString) {
+//        try {
+//            return PieceType.valueOf(pieceTypeString.toUpperCase());
+//        } catch (IllegalArgumentException e) {
+//            return PieceType.QUEEN;
+//        }
+//    }
 
     private void adjustSquareSize(double width, double height) {
         double squareSize = 0.8 * Math.min(width, height) / 8;
@@ -179,22 +222,28 @@ public class ChessBoardController {
                 StackPane squareStackPane = squareStackPanes[row][col];
                 squareStackPane.setPrefSize(squareSize, squareSize);
 
-                int squareStackSize = squareStackPane.getChildren().size();
-
-                for (int i = 0; i < squareStackSize; i++) {
-                    if (squareStackPane.getChildren().get(i) instanceof Rectangle rectangleLM) {
-                        rectangleLM.setWidth(squareSize);
-                        rectangleLM.setHeight(squareSize);
-                    } else if (squareStackPane.getChildren().get(i) instanceof Circle circleLP) {
-                        circleLP.setRadius(squareSize / 4);
-                    } else if (squareStackPane.getChildren().get(i) instanceof ImageView imgPiece) {
-                        imgPiece.setFitWidth(squareSize);
-                        imgPiece.setFitHeight(squareSize);
-                    }
-                }
+                adjustChildNodeSizes(squareStackPane, squareSize);
             }
         }
     }
 
+    private void adjustChildNodeSizes(StackPane squareStackPane,
+                                      double squareSize) {
+        int squareStackSize = squareStackPane.getChildren().size();
 
+        for (int i = 0; i < squareStackSize; i++) {
+            if (squareStackPane.getChildren().get(i)
+                    instanceof Rectangle rectangleLM) {
+                rectangleLM.setWidth(squareSize);
+                rectangleLM.setHeight(squareSize);
+            } else if (squareStackPane.getChildren().get(i)
+                    instanceof Circle circleLP) {
+                circleLP.setRadius(squareSize / 4);
+            } else if (squareStackPane.getChildren().get(i)
+                    instanceof ImageView imgPiece) {
+                imgPiece.setFitWidth(squareSize);
+                imgPiece.setFitHeight(squareSize);
+            }
+        }
+    }
 }
