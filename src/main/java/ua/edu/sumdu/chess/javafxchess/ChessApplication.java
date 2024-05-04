2b8f62b0a943e9a5de844d242c0ec4e336187758
atlantis-11 <85559317+atlantis-11@users.noreply.github.com>
2024-05-04 21:54:05 +0300

    Added fxml for start menu

--- src/main/java/ua/edu/sumdu/chess/javafxchess/ChessApplication.java
+++ src/main/java/ua/edu/sumdu/chess/javafxchess/ChessApplication.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import ua.edu.sumdu.chess.javafxchess.backend.Game;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -15,23 +14,20 @@ public class ChessApplication extends Application {
     public void start(Stage primaryStage) throws IOException {
         System.setProperty("prism.lcdtext", "false");
 
-        int timeInSeconds = 600;
-        Game game = new Game(timeInSeconds);
-
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainWindow.fxml"));
-        loader.setControllerFactory(c -> new MainWindowController(game));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("startWindow.fxml"));
+        loader.setControllerFactory(c -> new StartWindowController());
         Parent root = loader.load();
 
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root, 600, 400);
         String css = Objects.requireNonNull(getClass()
-            .getResource("/styles/mainWindow.css"))
+            .getResource("/styles/startWindow.css"))
             .toExternalForm();
         scene.getStylesheets().add(css);
 
-        primaryStage.setTitle("Chess");
+        primaryStage.setTitle("Start Menu");
         primaryStage.setScene(scene);
-        primaryStage.setMinWidth(400);
-        primaryStage.setMinHeight(450);
+        primaryStage.setMinWidth(480);
+        primaryStage.setMinHeight(250);
         primaryStage.show();
     }
 
